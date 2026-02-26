953d166cf55c6dd7467da4548e9ef23196281005
Markkusriis <xfj774@alumni.ku.dk>
2026-02-26 18:34:24 +0100

    Changed preprocessing significantly
    Forbedret funktionerne ved at anvende pandas, samt mere grundig clean_text funktion (tidligere rm_punctuation

--- src/pipeline/preprocessing.py
+++ src/pipeline/preprocessing.py
@@ -1,34 +1,112 @@
 import re
-import pandas as pd
-import nltk
 from nltk.corpus import stopwords
 from nltk.stem import SnowballStemmer
-nltk.download('punkt_tab')
-nltk.download("stopwords")
 
-def rm_punctuation(texts):
-    '''Takes pandas series as input and
-    removes all non-word and non-space characters and converts
-    all word characters to lowercase'''
-    non_word_non_space_patt = re.compile(r'[^\w\s]')
-    cleaned = texts.copy()
+
+def clean_text(articles):
+    '''
+    Takes pandas string Series as input and cleans all article elements
+
+    Parameters:
+        articles - Pandas Series of strings
+
+    Returns:
+        Cleaned articles. Removes whitespace, non word/space characters,
+        and tokenizes numbers, dates, URLs, and emails
+    '''
+    months = [r'january', r'february', r'march', r'april', r'may', r'june',
+              r'july', r'august', r'september', r'october', r'november', r'december',
+              r'jan', r'feb', r'mar', r'apr', r'jun', r'jul',
+              r'aug', r'sep', r'oct', r'nov', r'dec']
+    month_pattern = r'(' + '|'.join(months) + r')'
+    ddmmyy_patt = r'(\d{1,2}-\d{1,2}-\d{2,4})|(\d{1,2}/\d{1,2}/\d{2,4})\W'
+    m_d_y_patt = month_pattern + r'\s\d{1,2}(st|nd|rd|th)?,?\s\d{4}\W'
+    d_m_y_patt = r'\s\d{1,2}(st|nd|rd|th)?\s(of\s)?' + month_pattern + r',?\s\d{4}\W'
+    m_d_patt = month_pattern + r',?\s\d{1,2}(st|nd|rd|th)?\W'
+    d_m_patt =  r'\s\d{1,2}(st|nd|rd|th)?\s(of\s)?' + month_pattern
+    date_patts = [ddmmyy_patt, m_d_y_patt, d_m_y_patt, m_d_patt, d_m_patt]
+
+    patterns = {
+        'whitespace': re.compile(r'\s+'),
+        'email': re.compile(r'\S+(?![^\w@])\w@\S+(?![^\.])\.\w{2,}(\.\w{2,})?'),
+        'url': re.compile(r'(http\S+)|(\S+(?![^\w\.])\w\.\w{2,}(\.\w{2,})?(\S+)?)'),
+        'clean_bef_date': r'[^\w\s<>/-]',
+        'date': re.compile(r'(' + r')|('.join(date_patts) + r')'),
+        'non_word_non_space': re.compile(r'[^\w\s<>]'),
+        'num': re.compile(r'\d+')
+    }
+
+    cleaned = articles.copy()
+    cleaned = cleaned.str.replace(pat=patterns['whitespace'], repl=r' ', regex=True)
     cleaned = cleaned.str.lower()
-    cleaned = cleaned.str.replace(pat=non_word_non_space_patt, repl='', regex=True)
+    cleaned = cleaned.str.replace(pat=patterns['email'], repl=r'<EMAIL>', regex=True)
+    cleaned = cleaned.str.replace(pat=patterns['url'], repl=r'<URL>', regex=True)
+    cleaned = cleaned.str.replace(pat=patterns['clean_bef_date'], repl=r'', regex=True)
+    cleaned = cleaned.str.replace(pat=patterns['date'], repl=r'<DATE>', regex=True)
+    cleaned = cleaned.str.replace(pat=patterns['non_word_non_space'], repl=r'', regex=True)
+    cleaned = cleaned.str.replace(pat=patterns['whitespace'], repl=r' ', regex=True)
+    cleaned = cleaned.str.replace(pat=patterns['num'], repl=r'<NUM>', regex=True)
     return cleaned
 
-def to_tokens(texts): # parameter texts is a Pandas Series
-    tokens = [token for text in texts for token in nltk.word_tokenize(text)]
-    return tokens
 
-def rm_stopwords_from_tokens(tokens: list[str]):
-    stop_words = set(stopwords.words("english"))
-    stop_words -= {"no", "nor", "not"}
-    tokens_no_sw = [word for word in tokens if word not in stop_words]
-    return tokens_no_sw
+def to_tokens(articles): #
+    '''
+    Get tokens from collection of articles
 
-def stem_tokens(tokens: list[str]):
+    Parameters:
+        articles - Pandas Series of longer strings
+
+    Returns:
+        Pandas Series of tokens
+    '''
+    return articles.str.split(r'\s+').explode()
+
+
+def rm_stopwords_from_tokens(tokens):
+    '''
+    Remove stopwords from array of tokens using nltk.corpus.stopwords
+
+    Parameters:
+        tokens - Pandas Series of tokens
+
+    Returns:
+        Pandas Series of tokens without stopwords
+    '''
+    stop_words = set(stopwords.words("english")) - {"no", "nor", "not"}
+    tokens = tokens.astype("category")
+    return tokens[~tokens.isin(stop_words)]
+
+def stem_tokens(tokens):
+    '''
+    Stems pandas Series of tokens using nltk.stem.SnowBallStemmer
+
+    Paramters:
+        tokens - Pandas Series of tokens
+
+    Returns:
+        Pandas Series where stemming has been applied to every element
+    '''
     stemmer = SnowballStemmer("english")
-    return [stemmer.stem(token) for token in tokens]
+    return tokens.map(stemmer.stem)
 
-def get_size(tokens: list[str]):
-    return sum([len(token) for token in tokens])
+def get_size(tokens):
+    '''
+    Get total number of characters of all elements in a Pandas Series
+    '''
+    return tokens.str.len().sum()
+
+def preprocess(articles):
+    '''
+    Combined function of all functions in preprocessing module
+    '''
+    cleaned = clean_text(articles)
+
+    tokens = cleaned.str.split(r'\s+').explode()
+
+    stop_words = set(stopwords.words("english")) - {"no", "nor", "not"}
+    tokens = tokens[~tokens.isin(stop_words)]
+
+    stemmer = SnowballStemmer("english")
+    tokens = tokens.map(stemmer.stem)
+
+    return tokens
