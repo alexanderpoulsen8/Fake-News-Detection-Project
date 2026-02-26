2b8386b86fd27e9c7588af1046964f81293d7e99
alexanderpoulsen8 <alexanderpoulsen8@gmail.com>
2026-02-26 14:37:32 +0100

    updated subset_test

--- src/subset_test.py
+++ src/subset_test.py
@@ -1,4 +1,11 @@
 import pandas as pd
 
-df = pd.read_csv("/Users/alexanderpoulsen/Downloads/995,000_rows.csv")
+df = pd.read_csv(
+    "/Users/alexanderpoulsen/Downloads/995,000_rows.csv",
+    nrows=10000,
+    dtype={"id": str, "title": str, "author": str, "text": str, "label": int}
+)
 df.head()
+
+# print head
+print(df.head())
