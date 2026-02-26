c30d8435700b716c104ad09a806e0101161b781c
Markkusriis <xfj774@alumni.ku.dk>
2026-02-26 15:23:04 +0100

    changed stuff

--- src/preprocessing_test.py
+++ src/preprocessing_test.py
@@ -2,7 +2,7 @@ import preprocessing as preproc
 import pandas as pd
 
 
-df = pd.read_csv('news_sample.csv')
+df = pd.read_csv('../news_sample.csv')
 tokens = preproc.to_tokens(df['content'])
 print(preproc.get_size(tokens))
 df['content'] = preproc.rm_punctuation(df['content'])
