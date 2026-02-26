1b5794b0d2a82460d29297e8cd191d703384dff4
Markkusriis <xfj774@alumni.ku.dk>
2026-02-26 14:58:51 +0100

    Update preprocessing.py

--- src/preprocessing.py
+++ src/preprocessing.py
@@ -2,20 +2,36 @@ import re
 import numpy as np
 import pandas as pd
 import nltk
+from nltk.corpus import stopwords
 nltk.download('punkt_tab')
+nltk.download("stopwords")
 
-def clean_text(texts):
-    whitespace_patt = re.compile(r'\s+')
-    non_word_non_space_patt = re.compile(r'[^\w\s<>]')
-    cleaned = texts.copy()
-    cleaned = cleaned.str.replace(pat=whitespace_patt, repl=r' ', regex=True)
-    cleaned = cleaned.str.lower()
-    cleaned = cleaned.str.replace(pat=non_word_non_space_patt, repl='', regex=True)
-    return cleaned
+
 
 def to_tokens(texts): # parameter texts is a Pandas Series
     tokens = [token for text in texts for token in nltk.word_tokenize(text)]
     return tokens
 
+
+def rm_punctuation(texts):
+    non_word_non_space_patt = re.compile(r'[^\w\s]')
+    cleaned = texts.copy()
+    cleaned = cleaned.str.lower()
+    cleaned = cleaned.str.replace(pat=non_word_non_space_patt, repl='', regex=True)
+    return cleaned
+
+def rm_stopwords(tokens):
+    stop_words = set(stopwords.words("english"))
+    stop_words -= {"no", "nor", "not"}
+    tokens_no_sw = [word for word in tokens if word not in stop_words]
+    return tokens_no_sw
+
+
 df = pd.read_csv('news_sample.csv')
-print(to_tokens(clean_text(df['content']))[:100])
+tokens = to_tokens(df['content'])
+print(len(tokens))
+df['content'] = rm_punctuation(df['content'])
+tokens = to_tokens(df['content'])
+print(len(tokens))
+tokens = rm_stopwords(tokens)
+print(len(tokens))
