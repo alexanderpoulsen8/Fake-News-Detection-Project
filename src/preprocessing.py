4dd8c8da11b6e19122b2c66bd86603c3c2a698e7
Markkusriis <xfj774@alumni.ku.dk>
2026-02-26 15:07:32 +0100

    Update preprocessing.py

--- src/preprocessing.py
+++ src/preprocessing.py
@@ -6,21 +6,22 @@ from nltk.corpus import stopwords
 nltk.download('punkt_tab')
 nltk.download("stopwords")
 
-
-def to_tokens(texts): # parameter texts is a Pandas Series
-    tokens = [token for text in texts for token in nltk.word_tokenize(text)]
-    return tokens
-
-
 def rm_punctuation(texts):
+    '''Takes pandas series as input and
+    removes all non-word and non-space characters'''
     non_word_non_space_patt = re.compile(r'[^\w\s]')
     cleaned = texts.copy()
     cleaned = cleaned.str.lower()
     cleaned = cleaned.str.replace(pat=non_word_non_space_patt, repl='', regex=True)
     return cleaned
 
+def to_tokens(texts): # parameter texts is a Pandas Series
+    tokens = [token for text in texts for token in nltk.word_tokenize(text)]
+    return tokens
+
 def rm_stopwords(tokens):
     stop_words = set(stopwords.words("english"))
     stop_words -= {"no", "nor", "not"}
     tokens_no_sw = [word for word in tokens if word not in stop_words]
     return tokens_no_sw
+
