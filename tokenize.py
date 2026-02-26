89fcbbc023d314532747b13d79b6cab18aa12c8f
Markkusriis <xfj774@alumni.ku.dk>
2026-02-26 13:39:18 +0100

    Update tokenize.py

--- tokenize.py
+++ tokenize.py
@@ -6,6 +6,7 @@ import pandas as pd
 
 def tokenize(texts): # parameter texts is a Pandas Series
     whitespace_patt = re.compile(r'\s+')
+    non_word_non_space_patt = re.compile(r'[^\w\s<>]'),
     cleaned = texts.copy()
     cleaned = cleaned.str.replace(pat=whitespace_patt, repl=r' ', regex=True)
     cleaned = cleaned.str.lower()
