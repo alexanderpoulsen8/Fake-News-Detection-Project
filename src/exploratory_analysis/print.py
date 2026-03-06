acbaec342d3e2cafec96f9d7966bf1f939e63035
Markkusriis <xfj774@alumni.ku.dk>
2026-03-06 22:07:16 +0100

    Update print.py

--- src/exploratory_analysis/print.py
+++ src/exploratory_analysis/print.py
@@ -1,8 +1,7 @@
 import pandas as pd
 
-_FILEPATH = r"C:\Users\45515\OneDrive\Desktop\Studie\2_Semester_KU\GDS\Exam\Fake-News-Detection-Project\data\preprocessed_dataset.csv"
-_COLS = pd.read_csv(_FILEPATH, nrows=100)
+_FILEPATH = r"C:\Users\45515\OneDrive\Desktop\Studie\2_Semester_KU\GDS\Exam\Fake-News-Detection-Project\data\vocabulary_stats.csv"
+_COLS = pd.read_csv(_FILEPATH)
 
 # for keyword in _COLS['domain']:
 #     print(keyword)
-print(_COLS.columns)
