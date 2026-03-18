e36656c2d0e381efecfb7488d413ce933edb4c11
Markkusriis <xfj774@alumni.ku.dk>
2026-03-18 18:22:52 +0100

    Update read_big_fckn_csv.py

--- src/pipeline/read_big_fckn_csv.py
+++ src/pipeline/read_big_fckn_csv.py
@@ -8,7 +8,7 @@ _OUTPUT_PATH = StartPath / "data" / "full_preprocessed_dataset.csv"
 _FILEPATH = StartPath / "data" / "news_cleaned_2018_02_13.csv"
 
 _CHUNKSIZE = 20000
-_N_WORKERS = max(cpu_count() - 1, 1)
+_N_WORKERS = max(cpu_count() - 2, 1)
 
 def process_chunk(chunk):
     chunk["content_clean"] = preprocess_for_vectorizer(chunk["content"])
@@ -31,7 +31,7 @@ def main():
 
     with Pool(_N_WORKERS) as pool:
         print(f"Starting process using {_N_WORKERS} CPU cores")
-        for i, processed_chunk in enumerate(pool.imap(process_chunk, reader, chunksize=1), 1):
+        for i, processed_chunk in enumerate(pool.map(process_chunk, reader, chunksize=1), 1):
             print(f"chunk {i} done")
             processed_chunk.to_csv(
                 _OUTPUT_PATH,
