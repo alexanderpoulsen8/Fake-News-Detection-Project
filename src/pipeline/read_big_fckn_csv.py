a79fdc51793a995207ede948c5a1fa90a8298083
Markkusriis <xfj774@alumni.ku.dk>
2026-03-26 16:18:44 +0100

    Prepocessed 995,000.csv for vectorizer

--- src/pipeline/read_big_fckn_csv.py
+++ src/pipeline/read_big_fckn_csv.py
@@ -1,5 +1,5 @@
 import pandas as pd
-from preprocessing import preprocess
+from preprocessing import preprocess_for_vectorizer
 from multiprocessing import Pool, cpu_count
 from pathlib import Path
 
@@ -9,20 +9,20 @@ _SMALL_FILEPATH = data_dir / 'small_dataset' / 'news_sample.csv'
 _FILEPATH = data_dir / 'medium_dataset' / "995,000_rows.csv"
 _BIG_FILEPATH = data_dir / 'big_dataset' / "news_cleaned_2018_02_13.csv"
 _SMALL_OUTPUT_PATH = data_dir / 'small_dataset' / 'small_preprocessed_dataset.csv'
-_OUTPUT_PATH = data_dir / 'medium_dataset' / "preprocessed_dataset.csv"
+_OUTPUT_PATH = data_dir / 'medium_dataset' / "preprocessed_for_vectorizer_dataset.csv"
 _BIG_OUTPUT_PATH = data_dir / 'big_dataset' / "full_preprocessed_dataset.csv"
 
-_CHUNKSIZE = 10000
-_N_WORKERS = max(cpu_count() - 3, 1)
+_CHUNKSIZE = 20000
+_N_WORKERS = max(cpu_count() - 1, 1)
 
 def process_chunk(chunk):
-    chunk['content'] = preprocess(chunk["content"])
+    chunk['content'] = preprocess_for_vectorizer(chunk["content"])
     return chunk
 
 
 def preprocess_dataset(
-    filepath=_SMALL_FILEPATH,
-    output_path=_SMALL_OUTPUT_PATH,
+    filepath=_FILEPATH,
+    output_path=_OUTPUT_PATH,
     chunksize=_CHUNKSIZE,
     n_workers=_N_WORKERS
 ):
